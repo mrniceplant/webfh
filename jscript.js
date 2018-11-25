cbb890db032e98454416deaa251e117a06f1e2d0
mrniceplant <45319830+mrniceplant@users.noreply.github.com>
2018-11-25 14:01:48 +0100

    Create jscript.js

---
+++ jscript.js
@@ -0,0 +1,10 @@
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+/* <script>
+$(document).ready(function(){
+    $("body").click(function(){
+        $(this).hide();
+    });
+});
+</script> */
